55d74ffbb8d0ee77f3f1400715e6b80a96cc28a0
Maï Uchida <maiuchidakishaba@gmail.com>
2023-04-06 14:18:20 +0200

    Uploading the internet

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+//A funciton which adds the active class of panels
+panels.forEach((panel) => {
+    panel.addEventListener('click', () =>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+// A function which removes the active class of panels
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
