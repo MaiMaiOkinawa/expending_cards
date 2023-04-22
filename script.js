1325a86b22f07285d9387406240749ba23b8df10
Mai Uchida <maiuchidakishaba@gmail.com>
2023-04-23 01:28:57 +0200

    Experiment audio func

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll(".panel");
 
 //A funciton which adds the active class of panels
 panels.forEach((panel) => {
-    panel.addEventListener('click', () =>{
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
 
 // A function which removes the active class of panels
-function removeActiveClasses(){
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
